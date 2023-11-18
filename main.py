5d9e78f36a027bac116c85ec8db44b6223e97452
Ansuman Singh <ansumansingh@Ansumans-MacBook-Pro.local>
2023-11-18 02:30:18 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from schema import Author as SchemaAuthor
+from models import Author as ModelAuthor
+
+from fastapi_sqlalchemy import DBSessionMiddleware, db
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+app = FastAPI()
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+
+@app.get('/')
+def main_page():
+    return {'message': 'success'}
+
+@app.post('/add-author/', response_model=SchemaAuthor)
+def add_author(author:SchemaAuthor):
+    db_author = ModelAuthor(name=author.name, age=author.age)
+    db.session.add(db_author)
+    db.session.commit()
+    return db_author
